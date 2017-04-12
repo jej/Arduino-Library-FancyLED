8c732c7a624a2edd5b76b11c414fc444877abae3
Jej <jejc@free.fr>
2017-04-12 23:27:04 +0200

    Arduino 1.00+ compatible

--- FancyLED.cpp
+++ FancyLED.cpp
@@ -3,12 +3,16 @@
   (cc) 2011 Carlyn Maw, Attribute, Share Alike
   
   Created 06 July 2011
-  Version 0.1
+  Version 0.1.1
 */
 
 
 // include core Wiring API
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
 #include "WProgram.h"
+#endif
 
 // include this library's description file
 #include "FancyLED.h"
